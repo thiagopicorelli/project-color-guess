3b11098177e03d0af9f49af7dae1f296607adfd8
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-02 16:30:10 -0300

    Cria cores aleatórias a serem escolhidas

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const rgbColor = document.getElementById('rgb-color');
+const listaDeCores = document.getElementById('lista-de-cores');
 
 function getRandomRgb() {
-    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+  return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }
 
 let color = getRandomRgb();
-
 rgbColor.innerHTML = color.replace('rgb','');
+
+function createNewColor() {
+  const newColor = document.createElement('div');
+  newColor.className = 'ball';
+  newColor.style.backgroundColor = getRandomRgb();
+  listaDeCores.appendChild(newColor);
+}
+
+for(let i = 0; i < 6; i += 1) {
+  createNewColor();
+}
