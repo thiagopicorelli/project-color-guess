9d0cd798286994123906f42a3295bf03679c8904
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-02 16:53:22 -0300

    Adiciona a detecção da resposta certa ou errada

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const listaDeCores = document.getElementById('lista-de-cores');
 const answer = document.getElementById('answer');
 
 let color;
+let end = false;
 
 function getRandomRgb() {
   return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
@@ -12,10 +13,23 @@ function getRandomPosition() {
   return Math.floor(Math.random() * 6);
 }
 
+function endGame(event) {
+  if(end === true)
+    return;
+
+  if(event.target.style.backgroundColor === color) {
+    answer.innerHTML = 'Acertou!';
+  } else {
+    answer.innerHTML = 'Errou! Tente novamente!';
+  }
+  end = true;
+}
+
 function createNewColor(color) {
   const newColor = document.createElement('div');
   newColor.className = 'ball';
   newColor.style.backgroundColor = color;
+  newColor.addEventListener('click', endGame);
   listaDeCores.appendChild(newColor);
 }
 
