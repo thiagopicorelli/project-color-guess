e3190fd0b7ccd8ea5c70c3d1bf9b5989752eaff2
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-02 16:38:52 -0300

    Adiciona elemento de resposta

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const rgbColor = document.getElementById('rgb-color');
 const listaDeCores = document.getElementById('lista-de-cores');
+const answer = document.getElementById('answer');
+
+let color;
 
 function getRandomRgb() {
   return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }
-
-let color = getRandomRgb();
-rgbColor.innerHTML = color.replace('rgb','');
 
 function createNewColor() {
   const newColor = document.createElement('div');
@@ -15,6 +15,14 @@ function createNewColor() {
   listaDeCores.appendChild(newColor);
 }
 
-for(let i = 0; i < 6; i += 1) {
-  createNewColor();
+function startGame() {
+  color = getRandomRgb();
+  rgbColor.innerHTML = color.replace('rgb','');
+  answer.innerHTML = 'Escolha uma cor';
+
+  for(let i = 0; i < 6; i += 1) {
+    createNewColor();
+  }
 }
+
+startGame();
